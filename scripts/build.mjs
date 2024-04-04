315cd432af0064bafd4e76a83c33b4d406efc5ce
Nils Müller <tyriis@users.noreply.github.com>
2024-04-04 22:03:57 +0200

    feat(build): add build script

---
+++ scripts/build.mjs
@@ -0,0 +1,39 @@
+import "zx/globals"
+import fs from "fs"
+import json5 from "json5"
+import merge from "deepmerge"
+/* global $ echo */
+
+$.verbose = false
+
+// read json5 entry point
+// parse imports and merge into single object
+// run recursice function to merge all imports
+
+let parse = (entryPoint) => {
+  let data = fs.readFileSync(entryPoint, "utf8")
+  let parsed = json5.parse(data)
+  let result = { ...parsed }
+  if (parsed.extends) {
+    let githubRefs = parsed.extends.filter((extend) => extend.indexOf("github>") >= 0)
+    for (let githubRef of githubRefs) {
+      // expect gitubRef to be local
+      let path = githubRef.split("//")[1]
+      if (path.indexOf(".json") < 0) {
+        path += ".json"
+      }
+      const data = parse(path)
+      result = merge(result, data)
+    }
+  }
+  if (result.extends) {
+    result.extends = result.extends.filter((extend) => extend.indexOf("github>") < 0)
+  }
+  return result
+}
+
+let entryPoint = "flux-infra.json5"
+
+let result = parse(entryPoint)
+
+echo`${json5.stringify(result, null, 2)}`
